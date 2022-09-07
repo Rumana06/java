7c8b6bf07a21bf663c47e06df43f1aa45b6bbd6b
Rumana06 <113028661+Rumana06@users.noreply.github.com>
2022-09-07 17:52:42 +0530

    Add files via upload

---
+++ practice/Arthematic.java
@@ -0,0 +1,18 @@
+//arthematic operator +,-,*,/,%(modulus)
+
+class Arthematic
+{
+    public static void main(String[] args)
+    {
+        int x,y,z,m,n,p,q;
+        x=10;
+        y=3;
+        z=x+y;
+        m=x-y;
+        n=x*y;
+        p=x/y;
+        q=x%y;
+
+        System.out.println(z+"  "+m+"  "+n+" "+p+" "+q);
+    }
+}
