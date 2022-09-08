48d27531cb5a4f8cc88f72029fa13133935a39c0
Rumana06 <113028661+Rumana06@users.noreply.github.com>
2022-09-08 20:56:06 +0530

    Add files via upload

---
+++ practice/Datatypes.java
@@ -0,0 +1,28 @@
+//write a java code to read byte,short,int,long double,float,char,string,boolean 
+
+class Datatypes 
+ {
+   public static void main(String args[])
+   { 
+    byte myByte= 55;
+    short myShort= 1200;
+    int myInt= 45000;
+    long myLong= 2200000;
+    float myFloat= 3.99f;
+    double myDouble=2.23;
+    char myChar= 'r';
+    boolean myBoolean= true;
+    String myText= "Hi!";
+    
+   System.out.println("Byte = "+myByte);
+   System.out.println("Short = "+myShort);
+   System.out.println("Int = "+myInt);
+   System.out.println("Long = "+myLong);
+   System.out.println("Float = "+myFloat);
+   System.out.println("Double= "+myDouble);
+   System.out.println("Char = "+myChar);
+   System.out.println("Boolean = "+myBoolean);
+   System.out.println("String = "+myText);
+   
+   }
+ }
